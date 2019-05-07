4ba34fe139d43811eac0ba207ee22e35f07d9749
jdyepes <jesusyepes.1205@gmail.com>
2019-05-06 21:25:26 -0400

    Creando vista Avance de efectivo

---
+++ appSideMenu/src/app/avance-de-efectivo/avance-de-efectivo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avance-de-efectivo',
+  templateUrl: './avance-de-efectivo.page.html',
+  styleUrls: ['./avance-de-efectivo.page.scss'],
+})
+export class AvanceDeEfectivoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
