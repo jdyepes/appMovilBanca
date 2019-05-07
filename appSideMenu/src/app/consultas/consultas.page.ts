09a4248333235902857217027cf84dd9885391af
jesus <jesusyepes.1205@gmail.com>
2019-05-07 12:01:57 -0400

    Agregando componentes en modulo de Consultas

--- appSideMenu/src/app/consultas/consultas.page.ts
+++ appSideMenu/src/app/consultas/consultas.page.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ConsultasPage implements OnInit {
 
   constructor() { }
-
+  options: any[] = [
+    {
+      id: 1,
+      name: 'a',
+    },
+    {
+      id: 2,
+      name: 'b',
+    }
+  ]
   ngOnInit() {
   }
 
