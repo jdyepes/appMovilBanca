80d192600192d6a736e3c46c0846e8ab1429d1c1
jesus <jesusyepes.1205@gmail.com>
2019-05-24 10:12:41 -0400

    Agregando componente de Avance de efectivo

--- appSideMenu/src/app/avance-de-efectivo/avance-de-efectivo.page.ts
+++ appSideMenu/src/app/avance-de-efectivo/avance-de-efectivo.page.ts
@@ -12,4 +12,37 @@ export class AvanceDeEfectivoPage implements OnInit {
   ngOnInit() {
   }
 
+  accounts: any[] = [
+    {
+      id: 1,
+      name: 'Cuenta Corriente',
+    },
+    {
+      id: 2,
+      name: 'Cuenta de Ahorros',
+    }
+  ]
+
+  options: any[] = [
+    {
+      id: 1,
+      name: 'a',
+    },
+    {
+      id: 2,
+      name: 'b',
+    }
+  ]
+
+  cards: any[] = [
+    {
+      id: 'V1',
+      name: 'Visa',
+    },
+    {
+      id: 'M1',
+      name: 'Master Card',
+    }
+  ]
+
 }
