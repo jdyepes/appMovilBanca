91f76d69971fe88894b7e1c29ac1ab9cf5a5d5fc
jesus <jesusyepes.1205@gmail.com>
2019-06-13 16:49:08 -0400

    Colocando en el menu principal las opciones
    Home page

--- appSideMenu/src/app/home/home.page.ts
+++ appSideMenu/src/app/home/home.page.ts
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public appPages = [ 
+    {
+      title: 'Consultas',
+      url: '/consultas',
+      icon: 'search'
+    },
+    {
+      title: 'Transferencias',
+      url: '/transferencias',
+      icon: '',
+      src: '/assets/icon/transfer.svg'
+    },
+    {
+      title: 'Pago Móvil',
+      url: '/pago-movil',
+      icon: 'pago-movil',
+      src: '/assets/icon/metodo-de-pago.svg'
+    },
+    {
+      title: 'Pagos de TDC',
+      url: '/pago-tarjeta-de-credito',
+      icon: 'card'
+    },
+    {
+      title: 'Avance de Efectivo',
+      url: '/avance-de-efectivo',
+      icon: 'cash'
+    },
+    {
+      title: 'Recargas',
+      url: '/recargas',
+      icon: 'bookmarks'
+    },
+    {
+      title: 'Suspension de TDD',
+      url: '/suspension-tarjeta-de-debito',
+      icon: 'close-circle'
+    }
+  ];
+
 }
