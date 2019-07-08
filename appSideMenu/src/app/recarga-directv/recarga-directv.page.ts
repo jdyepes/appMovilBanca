741d2f249b8a10234b17663bf03e9269ae4d0380
jdyepes <jesusyepes.1205@gmail.com>
2019-07-07 21:04:57 -0400

    Recargas Directv y acomodando detalles de tamano y home
    Acomodando el tamano de los titulos de los cards.

--- appSideMenu/src/app/recarga-directv/recarga-directv.page.ts
+++ appSideMenu/src/app/recarga-directv/recarga-directv.page.ts
@@ -9,17 +9,21 @@ import { AlertController } from '@ionic/angular';
 export class RecargaDirectvPage implements OnInit {
 
   prefijoAccion: string;
-  operadoraSeleccion: string;
-  telefonoSeleccion: number;
-  tipoCuentaOrigen: string;
+  servicioSeleccion: any;
+  numeroClienteContrato: number|string;
+  tipoCuentaOrigen: any;
   correlativoOrigen: string;
   montoEntero: string;
   montoDecimal: string;
   operacion: string;
+  mostrar: boolean;
+  prepago: boolean;
+  mensajeEnviar: string;
 
   constructor(public alertCtrl: AlertController) {
     this.prefijoAccion = 'RD';
     this.operacion = 'RECARGA DIRECTV';
+    this.mostrar = false;
   }
 
   ngOnInit() {
@@ -38,18 +42,44 @@ export class RecargaDirectvPage implements OnInit {
     }
   ];
 
+  // tipo de servicios Directv
+  servicios: any[] = [
+    {
+      id: 1,
+      name: 'Prepago',
+    },
+    {
+      id: 2,
+      name: 'Previo Pago',
+    }
+  ];
+
   //correlativos
   options: number[] = [1, 2, 3, 4, 5, 6];
 
+  //evento del seleccionarServicio
+  mostrarContenido()
+  {
+    let aux = this.servicioSeleccion;
+    aux.id === 1 ? this.prepago = true : false; //prepago
+    aux.id === 2 ? this.prepago = false : true; //previo pago
+    this.mostrar = true; //oculta o no el contenido
+    this.numeroClienteContrato = '';
+    this.prefijoAccion = 'RD';
+  }
+
   //alertBox
   async realizarPago() {
+    let auxAccounts = this.tipoCuentaOrigen; // recibe la cuenta origen en objeto
+    let auxServicios = this.servicioSeleccion;
     let alert = await this.alertCtrl.create({
       header: 'Alerta',
       message: 'Confirma que desea realizar una ' + '<b>' + this.operacion + '</b>' +
         ' con los siguientes datos: ' + '<BR>' +
-        '<b>Teléfono: </b>' + this.operadoraSeleccion + this.telefonoSeleccion + '<BR>' +
+        '<b>Servicio: </b>' + auxServicios.name  + '<BR>' +
+        '<b>Nro Cliente/Contrato: </b>' + this.numeroClienteContrato + '<BR>' +
         '<b>Monto: </b>' + this.montoEntero + ',' + this.montoDecimal + '<BR>' +
-        '<b>Cuenta a debitar: </b>' + this.tipoCuentaOrigen + ' ' + this.correlativoOrigen,
+        '<b>Cuenta a debitar: </b>' + auxAccounts.name + ' ' + this.correlativoOrigen,
 
       buttons: [
         {
@@ -62,9 +92,15 @@ export class RecargaDirectvPage implements OnInit {
         {
           text: 'OK',
           handler: () => {
-            //si           
+            if (auxServicios.id === 1) {//Prepago
+              this.prefijoAccion = this.prefijoAccion + 'D';
+            }
+            if (auxServicios.id === 2) {//Previo Pago
+              this.prefijoAccion = this.prefijoAccion + 'P';
+            }
             // tslint:disable-next-line:max-line-length
-            console.log('mensaje a enviar: ' + this.prefijoAccion + ' ' + this.operadoraSeleccion + this.telefonoSeleccion + ' ' + this.montoEntero + ',' + this.montoDecimal + ' ' + this.tipoCuentaOrigen + this.correlativoOrigen);
+            this.mensajeEnviar = this.prefijoAccion + ' ' + this.numeroClienteContrato + ' ' + this.montoEntero + ',' + this.montoDecimal + ' ' + auxAccounts.shortCode + this.correlativoOrigen;
+            console.log('mensaje a enviar: ' + this.mensajeEnviar);
           }
         }
       ]
