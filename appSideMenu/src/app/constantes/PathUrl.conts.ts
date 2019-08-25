e781bb757f6370d153b49bdfe7e98c3e6c448427
jdyepes <jesusyepes.1205@gmail.com>
2019-08-25 03:27:43 -0400

    Se crea la carpeta de constantes
    Con el fin de colocar todos los prefijos de las operaciones y la url del servicio rest

---
+++ appSideMenu/src/app/constantes/PathUrl.conts.ts
@@ -0,0 +1,14 @@
+// Creado 3er Sprint 24/Agosto/2019
+// Url completa : http://localhost:60978/api/proveedor/obtenerProveedor
+
+export const PATH = {
+    PROVEEDOR: '/proveedor'
+};
+
+export const METHOD = {
+    ObtenerProveedor: '/obtenerProveedor'
+};
+
+export const AppUrlBase = {
+    appUrlBase: 'http://localhost:60978/api'
+};
---
+++ appSideMenu/src/app/constantes/prefijo-opciones.ts
@@ -0,0 +1,18 @@
+/** Constantes para el menu principal HOME
+ * Creado 25 Agosto 2019 fin 3er Sprint
+ */
+export const OPERACIONES = {
+    numeroDestinoProveedor:  '88232',
+    preSaldo:  'S',
+    preMovimiento:  'M',
+    preTransferencia:  'T',
+    prePagoTDC:  'P',
+    preAvanceEfectivo:  'A',
+    prePagoMovilPAT:  'PAT',
+    prePagoMovilPAC:  'PAC',
+    preRecargasTelefonicas:  'RT',
+    preRecargasDirectv:  'RD',
+    preDirectvPrePago:  'RDD',
+    preDirectvPrevioPago:  'RDP',
+    preSuspensionTDD:  'STD'
+}
