6da32b2e923525ac58a476fbb3c4b5ef9c67755c
jesus <jesusyepes.1205@gmail.com>
2019-09-06 09:25:07 -0400

    Creacion del servicio rest para la extraccion de la lista de los numeros de destino
    Se crea servicio rest api en c# visual studio 2013, teniendo los proveedores en el archivo de configuracion

---
+++ ServicioRestProveedorVS/WSProveedorSMSApi/WSProveedorSMSApi/Models/Proveedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSProveedorSMSApi.Models
+{
+    public class Proveedor
+    {      
+        public int _id { get; set; }
+        public String _nombre { get; set; }
+        public String _numero { get; set; }
+        public bool _disponible { get; set; }
+
+        public Proveedor(int id, String nombre, String numero, bool disponible)
+        {
+            _id = id;
+            _nombre = nombre;
+            _numero = numero;
+            _disponible = disponible;
+        }
+    }
+}
